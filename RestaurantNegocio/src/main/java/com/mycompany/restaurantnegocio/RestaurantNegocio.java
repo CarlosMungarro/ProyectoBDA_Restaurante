6ce3afe30bddb38becb84b78d4ef15088f0eb4ba
CarlosMungarro <carlos.mungarro247590@potros.itson.edu.mx>
2024-05-17 13:32:01 -0600

    Proyecto
    Capa Negocio, Persistencia y Presentacion creada

---
+++ RestaurantNegocio/src/main/java/com/mycompany/restaurantnegocio/RestaurantNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantnegocio;
+
+/**
+ *
+ * @author Carlo
+ */
+public class RestaurantNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ RestaurantPersistencia/src/main/java/com/mycompany/restaurantpersistencia/RestaurantPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.restaurantpersistencia;
+
+/**
+ *
+ * @author Carlo
+ */
+public class RestaurantPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
